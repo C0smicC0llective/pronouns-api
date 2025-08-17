2d861c177ef45f0c8d0d310cb0d251c2e52ec3e1
C0smicC0llective <evelynfoerster1@gmail.com>
2025-08-17 14:29:42 -0400

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,7 @@ import { createApp } from './app';
 import { config } from './config';
 
 const app = createApp(); // <-- THIS uses your configured app
-const PORT = process.env.PORT || 4000;
-
-const PORT = Number(process.env.PORT) || 4000;
+const PORT: number = Number(process.env.PORT) || 4000;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://localhost:${PORT}`);
