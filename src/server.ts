d769fbd4609a817b49a07b20afdd87133d448952
C0smicC0llective <evelynfoerster1@gmail.com>
2025-08-17 14:11:30 -0400

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,6 @@ const app = createApp(); // <-- THIS uses your configured app
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Pronouns API running on http://localhost:${PORT}`);
-    console.log(`API documentation available at http://localhost:${PORT}/api-docs`);
+  console.log(`Pronouns API running on http://localhost:${PORT}`);
+  console.log(`API documentation available at http://localhost:${PORT}/api-docs`);
 });
